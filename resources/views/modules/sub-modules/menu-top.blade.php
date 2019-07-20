35d6fd22ee46b17cd4a8ce454faf7d54e4f5051c
NguyenQui <nguyendinhqui1029@gmail.com>
2019-07-21 00:20:57 +0700

    menu hambeger

--- resources/views/modules/sub-modules/menu-top.blade.php
+++ resources/views/modules/sub-modules/menu-top.blade.php
@@ -18,7 +18,10 @@
         <div class="col-3 col-sm-3 col-md-2 col-lg-2 text-center">
             <a href="javascript:void(0)" class="register">Đăng ký thành viên</a>
         </div>
-        <div class="col-3 col-sm-3 col-md-2 col-lg-2 text-center">
+    <div class="col-2 col-sm-2 col-md-3 col-lg-3 text-center">
             <a href="javascript:void(0)" class="login">Đăng nhập</a>  
+        </div>
+        <div class="col-1 d-lg-none d-md-none d-block d-sm-block text-center">
+            <a href="javascript:void(0)" class="login"><i class="icon-align-justify" style="font-size: 30px;"></i></a>  
         </div>
 </div>
