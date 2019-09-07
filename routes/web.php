9c23d358b87a0350fe342614fcadc3e251d5d67f
phungtansang <phungtansang2312@gmail.com>
2019-09-07 16:05:07 +0700

    fix complit

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,10 @@ Route::get('/nha-moi-gioi',"NhaMoiGioiController@nhamoigioi" );
 
 Route::get('/kinh-nghiem',"KinhNghiemController@kinhnghiem" );
 Route::get('/mau-nha-dep',"MauNhaDepController@maunhadep" );
-<<<<<<< HEAD
 Route::get('/lienhe-gopy',"LienHeGopYController@lienhegopy" );
-=======
 Route::get('/nha-dat-ban',"NhaDatBanController@nhadatban" );
->>>>>>> 53cfb729a5ec476642c65a932eb4360f0a17744e
 
 Route::get('locale/{locale}', function($locale){
     Session::put('locale',$locale);
     return  redirect()->back();
 });
-<<<<<<< HEAD
-Route::get('/nha-dat-ban', function () {
-    return view('pages.nha-dat-ban');
-});
-
-=======
-
-
-// trang nhà mua giới
->>>>>>> 53cfb729a5ec476642c65a932eb4360f0a17744e
